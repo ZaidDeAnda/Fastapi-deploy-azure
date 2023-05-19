01cd6ae9f1c50afb7b3be7e21fc40cdc8dee841b
ZaidDeAnda <zaidthejedi@gmail.com>
2023-05-19 02:10:51 -0600

    dir changes

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import requests
 
-url = 'http://localhost:8000/predict'
-image_path = 'foto_1.jpeg'  # Ruta de la imagen que deseas enviar
+url = 'https://test-fastapi-tigrehacks.azurewebsites.net/predict'
+image_path = 'api/IMG_0009.JPG'  # Ruta de la imagen que deseas enviar
 
 with open(image_path, 'rb') as file:
     response = requests.post(url, files={'file': file})
 
+print(response)
 prediction = response.json()
 if 'cat' in prediction.values():
     print(f"En la imagen hay un {prediction['prediction']} :D")
