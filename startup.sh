69aef47036dbf78a4f36c2f30e26cf10b5804524
ZaidDeAnda <zaidthejedi@gmail.com>
2023-05-19 17:53:13 -0600

    added libglib2 to startup.sh

--- startup.sh
+++ startup.sh
@@ -5,6 +5,7 @@ apt-get update
 
 # Instala las dependencias necesarias
 apt-get install -y libgl1-mesa-glx
+apt-get install -y libglib2.0-0
 
 # Inicia la aplicación FastAPI
 uvicorn main:app --host 0.0.0.0 --port 8000
