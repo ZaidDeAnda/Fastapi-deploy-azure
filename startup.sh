d1b550c40c80276a066c2ce13eb1f461bdc42d95
ZaidDeAnda <zaidthejedi@gmail.com>
2023-05-19 17:01:49 -0600

    added libgl dependencies to startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,10 @@
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+#!/bin/bash
+
+# Actualiza los paquetes del sistema
+apt-get update
+
+# Instala las dependencias necesarias
+apt-get install -y libgl1-mesa-glx
+
+# Inicia la aplicación FastAPI
+uvicorn main:app --host 0.0.0.0 --port 8000
